221618ecd2352ccc05b27bf8fd6299a836499aeb
unknown <nismabanu012@gmail.com>
2024-11-06 12:25:06 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProductList from './pages/products';
+import { Link, Route, Routes } from 'react-router-dom';
+
+  function App() {
+    const [menu,setMenu]=useState(false);
+    function handleClick(){
+     setMenu(!menu);
+    }
+ 
+  return (
+    <div class="bg-amber-300 h-[100vh] pt-10">
+    <nav class="px-10 md:w-[90%] sm:w-[100%] md:mx-auto flex md:flex-row justify-between items-center">
+      <h3 class="text-3xl font-bold">Landoo</h3>
+      <div class="md:static absolute  md:min-h-fit min-h-[40vh] top-[-100%] left-0 md:w-auto w-full  flex items-center px-5">
+      <ul class='flex md:flex-row flex-col md:items-center  md:gap-[4vw]  space-x-2 text-lg'>
+      <li class='list-none hover:underline hover:underline-offset-8'><Link to={'/'}>Home</Link></li>
+      <li class='list-none hover:underline hover:underline-offset-8'>About Us</li>
+      <li class='list-none hover:underline hover:underline-offset-8'>Contact Us</li>
+      <li class='list-none hover:underline hover:underline-offset-8'><Link to={'/Products'}>Products</Link></li>
+      </ul>
+      </div>
+      <div class="px-5 font-bold md:space-x-4  items-center">
+        <button class="px-5 py-1 border-2 border-solid rounded-full border-stone-900 hover:bg-slate-800 hover:text-white">Log In</button>
+        <button class="px-5 py-1 border-2 border-solid rounded-full border-stone-900 hover:bg-black hover:text-white">Sign Up</button>
+         <img class="inline" src="https://img.icons8.com/?size=30&id=59832&format=png" onClick={handleClick}></img>
+      </div>
+      
+    </nav>{menu?
+      <div class="md:static absolute bg-black text-white  md:min-h-fit min-h-[40vh] top-[25%] left-0 md:w-auto w-full  flex items-center px-5">
+      <ul class='flex md:flex-row flex-col md:items-center  md:gap-[4vw]  space-x-2 text-lg'>
+      <li class='list-none hover:underline hover:underline-offset-8'>Home</li>
+      <li class='list-none hover:underline hover:underline-offset-8'>About Us</li>
+      <li class='list-none hover:underline hover:underline-offset-8'>Contact Us</li>
+      <li class='list-none hover:underline hover:underline-offset-8'>Products</li>
+      </ul>
+      </div>:null}
+      
+      <div class="md:pl-24 pl-12 my-28">
+      <h2 class="font-extrabold text-4xl md:w-2/3 lg:w-1/3 ">Style You with Our products</h2>
+      <div class="space-x-4 my-6">
+        <button class="bg-slate-800 text-white px-6 py-1 rounded-xl hover:bg-white hover:solid-border hover:border-stone-900 hover:border-2 hover:text-black">Shop Now</button>
+        <button class="bg-slate-800 text-white px-6 py-1 rounded-xl hover:bg-white hover:solid-border hover:border-stone-900 hover:border-2 hover:text-black">Get Offer</button>
+      </div>
+      </div>
+       <Routes>
+        <Route path="/products" element={<ProductList/>}></Route>
+        <Route path="/" element={<App/>}></Route>
+       </Routes>
+       <div class="bg-black w-[100%] relative bottom-0 h-12 flex justify-center items-center">
+         <p class="text-gray-300">@copyright
+         </p>
+       </div>
+
+
+      </div>
+      
+    
+  )
+
+}
+
+export default App
---
+++ src/pages/products.jsx
@@ -0,0 +1,61 @@
+function ProductList(){
+    return(
+        <div class="bg-amber-300">
+        <div class="flex md:flex-row flex-col md:w-[70%] lg:w-[70%] mx-auto gap-6" >
+            <div class="w-[90%] bg-white max-w-52 h-[25%] mx-auto flex flex-col my-5">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwJLCePZJwIG0PciR2JWB05lulEWd8X88a4g&s" class="w-[100%]"></img>
+                <h3 class="text-center font-bold text-2xl my-3">shirt</h3>
+                <div class="px-5 flex mb-5">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+                    <p class="text-red-700 font-bold text-xl">$250</p>
+                </div>
+                
+                <button class="bg-black px-5 py-2 font-bold text-amber-300 rouded
+                hover:opacity-30">
+                    Add to cart
+                </button>
+            </div>
+            <div class="w-[90%] bg-white max-w-52 h-[25%] mx-auto flex flex-col my-5">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwJLCePZJwIG0PciR2JWB05lulEWd8X88a4g&s" class="w-[100%]"></img>
+                <h3 class="text-center font-bold text-2xl my-3">shirt</h3>
+                <div class="px-5 flex mb-5">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+                    <p class="text-red-700 font-bold text-xl">$250</p>
+                </div>
+                
+                <button class="bg-black px-5 py-2 font-bold text-amber-300 rouded
+                hover:opacity-30">
+                    Add to cart
+                </button>
+            </div>
+            <div class="w-[90%] bg-white max-w-52 h-[25%] mx-auto flex flex-col my-5 ">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwJLCePZJwIG0PciR2JWB05lulEWd8X88a4g&s" class="w-[100%]"></img>
+                <h3 class="text-center font-bold text-2xl my-3">shirt</h3>
+                <div class="px-5 flex mb-5">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+                    <p class="text-red-700 font-bold text-xl">$250</p>
+                </div>
+                
+                <button class="bg-black px-5 py-2 font-bold text-amber-300 rouded
+                hover:opacity-30">
+                    Add to cart
+                </button>
+            </div>
+            <div class="w-[90%] bg-white max-w-52 h-[25%] mx-auto flex flex-col my-5 ">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwJLCePZJwIG0PciR2JWB05lulEWd8X88a4g&s" class="w-[100%]"></img>
+                <h3 class="text-center font-bold text-2xl my-3">shirt</h3>
+                <div class="px-5 flex mb-5">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+                    <p class="text-red-700 font-bold text-xl">$250</p>
+                </div>
+                
+                <button class="bg-black px-5 py-2 font-bold text-amber-300 rouded
+                hover:opacity-30">
+                    Add to cart
+                </button>
+            </div>
+            </div>
+        </div>
+    )
+}
+export default ProductList;
